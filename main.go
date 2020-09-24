c436fa3f6e7cdf9cf6a1affd9d65e6060fe22aba
Shazeb01 <shazebvps@gmail.com>
2020-09-24 06:59:21 -0700

    modified

--- main.go
+++ main.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/Shazeb01/golang-assig/pkg/config"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if len(port) == 0 {
-		port = "8000"
+	keys_array := [5]string{"port", "host", "connectionHost", "connectionDB", "connectionUser"}
+	for _, keys := range keys_array {
+		fmt.Println(keys + " " + config.Get(keys))
 	}
-	conf := config.Get(port)
-	fmt.Println(conf)
+
+	var set_value = map[string]string{
+		"port":           "8001",
+		"host":           "127.0.0.1",
+		"connectionHost": "db.mysecretapp.prod",
+		"connectionDB":   "testapp",
+		"connectionUser": "user",
+	}
+
+	for keys, values := range set_value {
+		config.Set(keys, values)
+		fmt.Println(keys + " " + config.Get(keys))
+	}
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,29 @@
 package config
 
+import (
+	"os"
+)
+
+var defs = map[string]string{
+	"port":           "8000",
+	"host":           "localhost",
+	"connectionHost": "db.mysecretapp.dev",
+	"connectionDB":   "secretapp",
+	"connectionUser": "root",
+}
+
 var cfg = make(map[string]string)
+
+func init() {
+	cfg = make(map[string]string)
+	for k, v := range defs {
+		env := os.Getenv(k)
+		if len(env) == 0 {
+			env = v
+		}
+		cfg[k] = env
+	}
+}
 
 func Get(key string) string {
 	return cfg[key]
